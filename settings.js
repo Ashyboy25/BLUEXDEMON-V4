9c8e549c80b503eb149e9324482e28e52871f01a
Ashyboy25 <ashergreen569@gmail.com>
2024-11-27 15:03:45 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "2347040093063"
  
-global.botNumber = ["2349019529423"]
+global.botNumber = ["2347040093063"]
 
 global.sign = "✘"
 // ☞ ➻ ➽ ☛ ✔
